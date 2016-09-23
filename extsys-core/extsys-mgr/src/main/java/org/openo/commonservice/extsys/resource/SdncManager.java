821690f90454141bde317a886a5d16c37a5457f0
sunqi310 <sun.qi310@zte.com.cn>
2016-09-23 17:22:32 +0800

    OCS-70 Modify SDNC  service uri
    Change-Id: Iecd786c5013177cb05cd4c5684641ece3c2606cc
    Issue-id: OCS-70
    Signed-off-by: sunqi310 <sun.qi310@zte.com.cn>

--- extsys-core/extsys-mgr/src/main/java/org/openo/commonservice/extsys/resource/SdncManager.java
+++ extsys-core/extsys-mgr/src/main/java/org/openo/commonservice/extsys/resource/SdncManager.java
@@ -48,7 +48,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/sdncs")
+@Path("/sdncontrollers")
 @Api(tags = {" sdnc Management     "})
 public class SdncManager {
 
